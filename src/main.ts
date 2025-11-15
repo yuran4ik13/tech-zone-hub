bbce33ffa7c22cb99c60827ba96f359944c6e7db
= <lebedevpavel0511@gmail.com>
2025-11-15 16:09:19 +0100

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,20 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: process.env.CORS_ORIGIN,
+      methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+      credentials: true,
+      preflightContinue: true,
+    });
+  } else {
+    app.enableCors({
+      origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+      methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+      credentials: true,
+    });
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
